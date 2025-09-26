c3d24462529248a053f48e5c8786fd4946bac862
Sander Vanryckeghem <78733641+SanderVanryckeghem@users.noreply.github.com>
2025-09-26 09:59:49 +0200

    Generate all component structure

---
+++ src/app/components/shared/particle-background/particle-background.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-particle-background',
+  imports: [],
+  templateUrl: './particle-background.html',
+  styleUrl: './particle-background.scss'
+})
+export class ParticleBackground {
+
+}
